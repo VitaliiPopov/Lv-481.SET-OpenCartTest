d222390c5b63d2c0c9edfba4e03d8a578351b189
Sofiya Yaropud <yaropudsophia@gmail.com>
2020-03-18 13:31:05 +0200

    Added fields for review.

--- OpencartTest/src/test/java/opencart/pages/product/ProductPage.java
+++ OpencartTest/src/test/java/opencart/pages/product/ProductPage.java
@@ -1,19 +1,41 @@
 package opencart.pages.product;
 
-import org.openqa.selenium.WebDriver;
 import opencart.pages.AbstractPageWithHeader;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 public class ProductPage extends AbstractPageWithHeader {
 
     private AvailableOptionsComponent availableOptionsComponent;
+    @FindBy(how = How.CSS, css = "a[href='#tab-review']")
+    private WebElement tabReviews;
+    @FindBy(how = How.XPATH, xpath = "//input[contains(@id,'input-name')]")
+    private WebElement inputFieldName;
+    @FindBy(how = How.XPATH, xpath = "//textarea[contains(@id,'input-review')]")
+    private WebElement inputFieldReview;
+    @FindBy(how=How.XPATH,xpath="//input[contains(@value,'1')]")
+    private WebElement checkboxUnderLow;
+    @FindBy(how=How.XPATH,xpath="//input[contains(@value,'2')]")
+    private WebElement checkboxLow;
+    @FindBy(how=How.XPATH,xpath="//input[contains(@value,'3')]")
+    private WebElement checkboxOk;
+    @FindBy(how=How.XPATH,xpath="//input[contains(@value,'4')]")
+    private WebElement checkboxHigh;
+    @FindBy(how=How.XPATH,xpath="//input[contains(@value,'5')]")
+    private WebElement checkboxWell;
+    @FindBy(how=How.XPATH,xpath="//button[contains(@id,'button-review')]")
+    private WebElement buttonAddReview;
 
     public ProductPage(WebDriver driver) {
         super(driver);
-        initElements();
+        PageFactory.initElements(driver,this);
     }
 
-    private void initElements(){
-        availableOptionsComponent = new AvailableOptionsComponent(driver);
-    }
+    //private void initElements(){
+        //availableOptionsComponent = new AvailableOptionsComponent(driver);
+    //}
 
 }

98cb3637cc228ebe5e6db52fef7610776f01d91d
VitaliiPopov <lorenzo.somalia.lxix@gmail.com>
2020-03-30 23:35:59 +0300

    add package test

---
+++ OpencartTest/src/com/opencart/tools/test_runner/CurrencyTestRunner.java
@@ -0,0 +1,36 @@
+package com.opencart.tools.test_runner;
+
+import com.opencart.pages.HomePage;
+import com.opencart.pages.cart.CartPage;
+import com.opencart.pages.wishlist.WishListPage;
+import com.opencart.tools.Driver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+public class CurrencyTestRunner {
+
+    @BeforeClass
+    public void beforeClass() {
+        Driver.getDriver();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        Driver.quit();
+    }
+
+
+    public HomePage getHomePage() {
+        return new HomePage(Driver.getDriver());
+    }
+
+    public CartPage getCartPage() {
+        return new CartPage(Driver.getDriver());
+    }
+
+    public WishListPage getWishListPage() {
+        return new WishListPage(Driver.getDriver());
+    }
+}

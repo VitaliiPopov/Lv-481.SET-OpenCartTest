2c5a18cdc4525c08847f80ae3e7c521031d5fb4f
Mykhailo Tsalan <pmtsmichael@gmail.com>
2020-03-31 02:18:48 +0300

    correct import

--- Rest_Demo/src/test/java/com/petstore/test/store/get_order/ResponseBodyTest.java
+++ Rest_Demo/src/test/java/com/petstore/test/store/get_order/ResponseBodyTest.java
@@ -1,7 +1,7 @@
 package com.petstore.test.store.get_order;
 
-import Entity.Order;
-import Entity.OrderRepository;
+import com.petstore.data.Order;
+import com.petstore.data.OrderRepository;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;

fdd18164ba6b3ea5b6e4de00be4bc5448819555c
Vitalii Popov <49288827+VitaliiPopov@users.noreply.github.com>
2020-03-31 03:45:12 +0300

    Update ConstantVariables.java

--- OpencartTest/src/com/opencart/data/ConstantVariables.java
+++ OpencartTest/src/com/opencart/data/ConstantVariables.java
@@ -4,14 +4,6 @@ import java.math.BigDecimal;
 
 public interface ConstantVariables {
     public static String BROWSER_NAME = "chrome";
-
-    //public static String BROWSER_NAME = "firefox";
-    //public static String BROWSER_NAME = "edge";
-    //
-//    public static final String URL = "http://52.232.108.109/";
-//    public static final String ADMIN_URL = "http://52.232.108.109/admin";
-//    public static final String CART_PAGE_URL = "http://52.232.108.109/index.php?route=checkout/cart";
-
     //
     public static final String URL = System.getenv("URL");
     public static final String ADMIN_URL = System.getenv("ADMIN_URL");

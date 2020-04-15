dc6793e4ae460f30e50e7df14caa2be141f13f33
VitaliiPopov <lorenzo.somalia.lxix@gmail.com>
2020-04-16 00:24:33 +0300

    change URLs, add constants

--- Rest_Demo/src/com.petstore/data/ConstantVariables.java
+++ Rest_Demo/src/com.petstore/data/ConstantVariables.java
@@ -5,4 +5,7 @@ public final class ConstantVariables {
     public static final String API_URL = System.getenv("API_URL");
     public static final int API_PORT = Integer.parseInt(System.getenv("API_PORT"));
     public static final String API_PATH = System.getenv("API_PATH");
+    
+    public static final String STORE_PATH = "/store/order";
+    public static final String STORE_ID = "/{id}";
 }
--- Rest_Demo/src/com.petstore/test/store/create_order/SendRequestTest.java
+++ Rest_Demo/src/com.petstore/test/store/create_order/SendRequestTest.java
@@ -1,5 +1,6 @@
 package com.petstore.test.store.create_order;
 
+import com.petstore.data.ConstantVariables;
 import com.petstore.data.Order;
 import com.petstore.data.OrderRepository;
 import io.restassured.RestAssured;
@@ -20,9 +21,9 @@ public class SendRequestTest {
     @BeforeClass
     public void setRequestSpec() {
         RequestSpecification requestSpec = new RequestSpecBuilder()
-                .setBaseUri("http://192.168.99.100/")
-                .setPort(8080)
-                .setBasePath("api/v3/store/order")
+                .setBaseUri(ConstantVariables.API_URL)
+                .setPort(ConstantVariables.API_PORT)
+                .setBasePath(ConstantVariables.API_PATH + ConstantVariables.STORE_PATH)
                 .setAccept(ContentType.JSON)
                 .setContentType(ContentType.JSON)
                 .log(LogDetail.ALL)
